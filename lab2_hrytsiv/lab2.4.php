53925b543d429f93ab4b51bfef649450d6b5632a
cirin0 <ivan.gruziv@gmail.com>
2024-02-22 15:40:25 +0200

    task completed 2.4 lab 2. Refactor

--- lab2_hrytsiv/lab2.4.php
+++ lab2_hrytsiv/lab2.4.php
@@ -8,65 +8,66 @@ require '../config.php';
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href="/css/style.css" rel="stylesheet">
+   <link href="./css/style.css" rel="stylesheet">
    <title>Лабораторна робота №2</title>
 </head>
 
 <body>
    <div class="wrapper">
       <h3>Виберіть, будь ласка, зображення кота разом з собакою</h3>
-      <div class="form_container form_task4">
+      <div class="form_container">
          <form id="SubmitForm" method="post">
-            <label>
-               <input type="radio" name="catDog">
-               <img src="./images/cat-with-dog.jpg" alt="Кіт з Собакою" onclick="submitForm()">
-            </label>
-            <label>
-               <input type="radio" name="capybara">
-               <img src="./images/capybara.jpg" alt="Капібара" onclick="submitForm()">
-            </label>
-            <label>
-               <input type="radio" name="ferret">
-               <img src="./images/ferret.jpg" alt="Тхір" onclick="submitForm()">
-            </label>
-            <label>
-               <input type="radio" name="kaban">
-               <img src="./images/kaban.jpg" alt="Кабан" onclick="submitForm()">
-            </label>
+            <div class="animal_container">
+               <input type="radio" name="animal" id="catDog" value="catDog">
+               <label for="catDog"><img src="./images/cat-with-dog.jpg" alt="Кіт з Собакою"></label>
+               <input type="radio" name="animal" id="capybara" value="capybara">
+               <label for="capybara"><img src="./images/capybara.jpg" alt="Капібара"></label>
+               <input type="radio" name="animal" id="ferret" value="ferret">
+               <label for="ferret"><img src="./images/ferret.jpg" alt="Тхір"></label>
+               <input type="radio" name="animal" id="сhinchilla" value="сhinchilla">
+               <label for="сhinchilla"><img src="./images/сhinchilla.jpg" alt="Шиншила"></label>
+            </div>
+            <button type="submit">Перевірити відповідь</button>
          </form>
       </div>
       <?php
-      $catDog = $_POST["catDog"];
-      $capybara = $_POST["capybara"];
-      $ferret = $_POST["ferret"];
-      $kaban = $_POST["kaban"];
-
-      $correctAnswer = "catDog";
-      switch ($selectedAnimal) {
-         case "catDog":
-         case "capybara":
-         case "ferret":
-         case "kaban":
-            $resultMessage = ($selectedAnimal === $correctAnswer) ? "Правильно!" : "Неправильно. Ви вибрали {$selectedAnimal}.";
-            break;
-         default:
-            $resultMessage = "Невірний вибір.";
+      if ($_SERVER["REQUEST_METHOD"] === "POST") {
+         if (isset($_POST["animal"])) {
+            $selectedAnimal = $_POST["animal"];
+            $correctAnswer = "catDog";
+            switch ($selectedAnimal) {
+               case "catDog":
+                  $selectedAnimalName = "Кота з Собакою";
+                  break;
+               case "capybara":
+                  $selectedAnimalName = "Капібару";
+                  break;
+               case "ferret":
+                  $selectedAnimalName = "Тхора";
+                  break;
+               case "сhinchilla":
+                  $selectedAnimalName = "Шинлилу";
+                  break;
+               default:
+                  $selectedAnimalName = "";
+            }
+            if ($selectedAnimal === $correctAnswer) {
+               echo "<p>Правильно! Ви вибрали {$selectedAnimalName}.<p>";
+               echo "<label><img style= \"width: 300px;\" src=\"./images/cat-with-dog.jpg\"></label>";
+            } else {
+               echo "<label><img style= \"width: 300px;\" src='./images/{$selectedAnimal}.jpg'></label>";
+               echo "<p>Неправильно. Ви вибрали {$selectedAnimalName}.<p>";
+            }
+         } else {
+            echo "<p>Будь ласка, зробіть вибір.</p>";
+         }
       }
-      echo "<p>{$resultMessage}</p>";
       ?>
       <div class="next_task">
          <a href="lab2.5.php">Завдання 5</a><br><br>
          <a href="/index.php">Головна</a>
       </div>
    </div>
-   <script>
-      // function submitForm() {
-      //    document.getElementById("SubmitForm").submit();
-      // }
-      function submitForm(value) {
-         document.querySelector('input[name="animal"]').value = value;
-         document.getElementById("SubmitForm").submit();
-      }
-   </script>
 </body>
 
 </html>
---
+++ lab2_hrytsiv/lab2.5.php
@@ -0,0 +1,30 @@
+<?php
+require '../config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="/css/style.css" rel="stylesheet">
+   <title>Лабораторна робота №2</title>
+</head>
+
+<body>
+   <div class="wrapper">
+      <h3>Виберіть, будь ласка, зображення кота разом з собакою</h3>
+      <div class="form_container form_task4">
+      </div>
+      <?php
+      ?>
+      <div class="next_task">
+         <a href="lab2.5.php">Завдання 5</a><br><br>
+         <a href="/index.php">Головна</a>
+      </div>
+   </div>
+
+   </script>
+</body>
+
+</html>
