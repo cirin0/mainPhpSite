b10cd4141f2e25e81a1d196983ba46ccc2c909f8
cirin0 <ivan.gruziv@gmail.com>
2024-03-08 01:11:33 +0200

    test fleet lab4

---
+++ lab4_hrytsiv/lab4.php
@@ -0,0 +1,26 @@
+<?php
+require '../config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="/css/style.css" rel="stylesheet">
+   <link href="./css/style.css" rel="stylesheet">
+   <title>Лабораторна робота №4</title>
+</head>
+
+<body>
+<div class="wrapper">
+
+   <div class="next_task">
+      <div>
+         <a href="lab4.2.php">Завдання 2 >></a>
+      </div>
+      <a href="/index.php">Головна</a>
+   </div>
+</div>
+</body>
+
+</html>
