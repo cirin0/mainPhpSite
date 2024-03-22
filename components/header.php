5d5786792326c37f2b4b3f2532255bfec5ed9ded
cirin0 <ivan.gruziv@gmail.com>
2024-03-23 00:05:57 +0200

    Add: display favico for all next page

--- components/header.php
+++ components/header.php
@@ -4,6 +4,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="shortcut icon" type="image/x-icon" href="/favico.ico">
    <link href="/css/style.css" rel="stylesheet">
    <link href="./css/style.css" rel="stylesheet">
    <title>Лабораторна робота №4</title>
