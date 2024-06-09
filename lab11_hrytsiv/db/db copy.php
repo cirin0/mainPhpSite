02da1acf088bcb39f326d141256444e4db16a023
cirin0 <61295734+cirin0@users.noreply.github.com>
2024-06-09 15:26:54 +0300

    Delete lab11_hrytsiv/db/db copy.php

--- lab11_hrytsiv/db/db copy.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_name = 'pet_store';
-$db_host = '127.0.0.1';
-$db_user = 'cirin';
-$db_pass = 'Q20012004q';
-
-$db_server = new mysqli("$db_host", "$db_user", "$db_pass", "$db_name");
-$db_server->set_charset("utf8");
